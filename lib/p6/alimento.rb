d1c96272badf91b51e85c60f271205093246e957
Carlos Díaz <alu0101102726@ull.edu.es>
2019-11-14 09:11:15 +0000

    Sexta edición código: Creando el getter de los lípidos | CORRECTO

--- lib/p6/alimento.rb
+++ lib/p6/alimento.rb
@@ -1,9 +1,9 @@
 class Alimento
-	attr_reader :nombre , :proteinas , :carbohidratos
+	attr_reader :nombre , :proteinas , :carbohidratos , :lipidos
 
 	def initialize(nom , prot , carbohid , lip , ge , ter)
 		@nombre = nom
-		@proteinas , @carbohidratos , @lipidos = prot , carbohid , prot
+		@proteinas , @carbohidratos , @lipidos = prot , carbohid , lip
 		@gei , @terreno = ge , ter
 	end	
 end
